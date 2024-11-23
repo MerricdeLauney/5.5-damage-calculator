f25139434b786c6f282e01d6e4d5e5a58875ce79
MerricdeLauney <mdelauney@vmware.com>
2024-11-22 18:17:25 -0800

    Attempt to test single attack with mocked dice roll

--- tests/test_creature.py
+++ tests/test_creature.py
@@ -10,14 +10,24 @@ test_dummy = Creature("test_dummy", 0, [], 10000, 0)
 rogue_attacks = [Attack(5, 3, [8, 6, 6])]
 rogue = Creature("player", 5, rogue_attacks, 24, 16, origin_advantage=True)
 
-
-
+basic_attack = Attack(5, 3 [8])
+basic_creature = Creature("player", 5, [basic_attack], 24, 16)
 
 @patch("random.randint")
 def test_rogue(randint_mock):
     randint_mock.return_value = 1
     rogue.take_attacks([test_dummy])
     assert rogue.total_damage_dealt == 9
+
+def _side_effect_randint(var1, var2):
+        return var2
+
+@patch('random.randint', side_effect='_side_effect_randint')
+def test_attack(randint_mock):
+    basic_attacker.attack(target_dummy)
+    assert rogue.total_damage_dealt == 9
+    
+    
 
 
 # def test_rogue():
